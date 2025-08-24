40664bea584101a1a28eff64e8812b4ba98e3664
JUNGHYUNJOO <hyunjoo@JUNGui-MacBookPro.local>
2025-08-24 13:30:19 +0900

    feat: add boards controller
    - boards controller 생성
    - NestCLI : $nest g controller boards --no-spec

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,6 @@
+// 컨트롤러 생성 : $nest g controller boards --no-spec
+// --no-spec : 테스트 코드 생성 X
+import { Controller } from '@nestjs/common';
+
+@Controller('boards')
+export class BoardsController {}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,6 +1,9 @@
 // Board Module 생성 : $nest g module boards
 import { Module } from '@nestjs/common';
+import { BoardsController } from './boards.controller';
 
-@Module({})
+@Module({
+  controllers: [BoardsController]
+})
 export class BoardsModule {}
 
